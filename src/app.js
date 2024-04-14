ec8de6816e3f2e062f3cec08acb8fc41edf30b23
Agustin Braco <bracoagustin@gmail.com>
2024-04-13 23:01:52 -0300

    [Main] - New endpoint to send emails from Manuel Suarez

--- src/app.js
+++ src/app.js
@@ -63,4 +63,52 @@ app.post('/', async (req, res) => {
 	return res.status(200).json({ status: 'Enviado', message: 'Gracias por tu mensaje' });
 });
 
+app.post('/cc', async (req, res) => {
+	const { api_key } = req.headers;
+	if (!api_key | (api_key !== APIKEY))
+		return res.status(403).json({ status: 'Error', message: 'No estás autorizado.' });
+
+	const { name, email, phone, message } = req.body;
+	if (!name | !email |!phone | !message)
+		return res.status(400).json({ status: 'Error', message: 'Faltan datos para enviar el mail.' });
+
+	const { data, error } = await resend.emails.send({
+		from: 'CC Manuel Suarez <onboarding@resend.dev>',
+		to: ['bracoagustin@gmail.com'],
+		subject: 'Contacto web',
+		html: `
+			<html lang="es">
+				<body style="background-color: #F5F5F4;">
+					<table width="100%" cellspacing="0" cellpadding="0">
+						<tr>
+							<td align="center">
+								<table width="600" cellspacing="0" cellpadding="0">
+									<tr>
+										<td align="center" style="padding: 20px 0;">
+											<h1 style="color: #333;">Contacto web</h1>
+										</td>
+									</tr>
+									<tr>
+										<td align="center" style="padding: 0 20px;">
+											<p style="color: #666;">Nombre: ${name}</p>
+											<p style="color: #666;">Correo: ${email}</p>
+											<p style="color: #666;">Celular: ${phone}</p>
+											<p style="color: #666;">Mensaje: ${message}</p>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</body>
+			</html>
+    `,
+	});
+
+	if (error)
+		return res.status(400).json({ status: 'Error', message: 'El mensaje no pudo ser enviado' });
+
+	return res.status(200).json({ status: 'Enviado', message: 'Gracias por tu mensaje' });
+});
+
 app.listen(3000);
